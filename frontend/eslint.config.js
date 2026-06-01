f17e40c830feb8cac53d758205b87143b99cbb22
Gufran Ahmed <gufranahmed0921@gmail.com>
2026-06-01 21:52:58 +0530

    Fix React lint configuration and lock dependencies

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,5 +1,6 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 
@@ -14,10 +15,12 @@ export default [
     },
     plugins: {
       'react-hooks': reactHooks,
+      react,
       'react-refresh': reactRefresh,
     },
     rules: {
       ...js.configs.recommended.rules,
+      'react/jsx-uses-vars': 'error',
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     },
